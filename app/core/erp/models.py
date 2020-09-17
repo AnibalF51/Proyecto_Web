334ea6f18e8f8b781c240f28376cd5b801ac4557
milo612 <mpopa2@miumg.edu.gt>
2020-09-16 23:56:21 -0600

    Tercer commit

--- app/core/erp/models.py
+++ app/core/erp/models.py
@@ -1,8 +1,33 @@
 from django.db import models
 from datetime import datetime
 
-# Create your models here.
+class Type(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Nombre', unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Tipo'
+        verbose_name_plural = 'Tipos'
+        db_table = 'type'
+        ordering = ['id']
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Nombre')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categoria'
+        db_table = 'category'
+        ordering = ['id']
+
 class Employee(models.Model):
+    categ = models.ManyToManyField(Category)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
     names = models.CharField(max_length=150, verbose_name='Nombres')
     dpi = models.CharField(max_length=10, unique=True, verbose_name='DPI')
     date_joined = models.DateField(default=datetime.now, verbose_name='Fecha de Registro')
@@ -21,5 +46,5 @@ class Employee(models.Model):
     class Meta:
         verbose_name = 'Empleado'
         verbose_name_plural = 'Empleados'
-        #db_table = 'empleado'
+        db_table = 'empleado'
         ordering = ['id']
--- app/core/erp/tests.py
+++ app/core/erp/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from config.asgi import *
+from core.erp.models import Type
 
-# Create your tests here.
+#Listar
+
+# select * from Tabla
+# query = Type.objects.all()
+# print(query)
+
+#insercion
+# t = Type(name='Alksjdkjs23').save()
+
+# t = Type()
+# t.name = 'Purekjas'
+# t.save()
+
+#edicion
+try:
+    t = Type.objects.get(pk=1)
+    t.name = 'Presidente'
+    t.save()
+except Exception as e:
+    print(e)
+
+#eliminacion
+# t = Type.objects.get(pk = 1)
+# t.delete()
