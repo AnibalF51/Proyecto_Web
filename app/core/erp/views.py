845b6fa0ad38aebd367e7edecee2c5cc66647eeb
milo612 <mpopa2@miumg.edu.gt>
2020-10-13 18:35:18 -0600

    video14

--- app/core/erp/views.py
+++ app/core/erp/views.py
@@ -1,3 +1,12 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+
+def myfirstview(request):
+    data = {
+        'name': 'William'
+    }
+
+    return JsonResponse(data)
